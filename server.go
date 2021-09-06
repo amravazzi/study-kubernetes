33f56a3424dfc7d9505a9a308472b99900fe0ce6
Andre Ravazzi <amravazzi@gmail.com>
2021-09-05 21:24:30 -0300

    update deployment

--- server.go
+++ server.go
@@ -6,14 +6,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/configmap", ConfigMap)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8000", nil)
 }
+
+var startedAt = time.Now()
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
@@ -33,4 +37,15 @@ func Secret(w http.ResponseWriter, r *http.Request) {
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
 	fmt.Fprintf(w, "User: %s. Passwrod %s.", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+	if duration.Seconds() < 10 || duration.Seconds() > 30 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
 }
